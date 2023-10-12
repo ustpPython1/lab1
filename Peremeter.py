c9e2eb00779d4213c3beb2a2c64b992fed0b80e9
Wilfred m. badayos <badayoswilfred246@gmai.com>
2023-10-12 05:53:27 -0700

    first commit

---
+++ Peremeter.py
@@ -0,0 +1,13 @@
+#Activity #2 Write a Python progress that will use funtion called calculate_perimeter
+#The perimeter of a rectangle can be calculated using the formula : perimeter = 2 *
+
+def calculate_perimeter(length + width)
+    perimeter = 2 * (legth + width)
+    return calculate_perimeter
+
+length = int(input("length of the rectangle"))
+width = int(input("width of the rectangle"))
+
+x = calculate_perimter (length, width)
+
+print ("The perimeter of a rectangle is ", x)
---
+++ diesel.py
@@ -0,0 +1,8 @@
+#Activity #1 Gasoline
+print ("Gasoline price is 70")
+Litters = int (input("how many litters of gasoline"))
+price_gasoline= 70 
+litters = Litters * price_gasoline
+
+print ("it will be p " + str(Litters))
+
